9717fb2beb5c1ca0788120a919088c917c11f4a3
YuriSFarias <yurisfarias@gmail.com>
2025-08-29 15:13:07 -0300

    clean typos

--- backend/customers/models.py
+++ backend/customers/models.py
@@ -1,9 +1,7 @@
-# customers/models.py (ou dentro do app que preferir)
 from django.conf import settings
 from django.db import models
-# from model_utils.models import TimeStampedModel  # se quiser timestamps
 
-class Customer(models.Model):  # ou TimeStampedModel
+class Customer(models.Model): 
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
